484c3bbaf95c1d670b88f2f7c62445246a0515b2
Niranga De Silva <nirangad@99x.io>
2024-11-15 00:25:15 +0530

    Cron scheduled for Dubai Midnight

--- src/tasks/tasks.cron.ts
+++ src/tasks/tasks.cron.ts
@@ -24,8 +24,7 @@ export class TasksCron {
   // @Cron(CronExpression.EVERY_30_SECONDS)
   // @Cron(CronExpression.EVERY_5_MINUTES)
   // @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-  // @Cron('30 21 * * *')
-  @Cron('25 18 * * *')
+  @Cron('30 21 * * *')
   async readPartnerProductFeedCronJob() {
     if (this.readProductFeedCronJobMutex) {
       this.logger.warn(
