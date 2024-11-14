fa1de74971a4d65aa934f654714720b0c81a37d4
Niranga De Silva <nirangad@99x.io>
2024-11-14 23:26:05 +0530

    Changed cron time

--- src/tasks/tasks.cron.ts
+++ src/tasks/tasks.cron.ts
@@ -24,7 +24,8 @@ export class TasksCron {
   // @Cron(CronExpression.EVERY_30_SECONDS)
   // @Cron(CronExpression.EVERY_5_MINUTES)
   // @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-  @Cron('30 21 * * *')
+  // @Cron('30 21 * * *')
+  @Cron('00 18 * * *')
   async readPartnerProductFeedCronJob() {
     if (this.readProductFeedCronJobMutex) {
       this.logger.warn(
